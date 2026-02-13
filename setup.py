1d05449f2c34a1d0e4933fd48d82ef4ccf6b859e
Ayush-12334 <ayush23.ghadai@gmail.com>
2026-02-13 15:46:21 +0530

    there was a error

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->list[str]:
 
-    reuirements=[]
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = [] # FIXED SPELLING HERE
     with open(file_path) as file:
-        requirements=file.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-
-
+            
+    return requirements # Ensure this is aligned with 'with'
 
 setup(
-
-    name='PROJECT 3.0',
+    name='PROJECT_3.0', # Tip: Avoid spaces in names (use _ or -)
     version='0.0.1',
     author='Ayush',
     author_email='ayush23.ghadai@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt")
-
 )
